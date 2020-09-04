e39cb871fc4b5964b27b427cb7e1e5f05279bcad
Asim <asimshrestha608@gmail.com>
2020-09-04 13:38:23 +0545

    build: Binary Search Algorithm

---
+++ binary_search.py
@@ -0,0 +1,26 @@
+from random import shuffle
+
+# creating list of numbers from 0 to 9.
+numbers_list = [i for i in range(10)]
+shuffle(numbers_list)  # shuffle the numbers list.
+
+# sorted_numbers_list = sorted(numbers_list)  # sort numbers list in ascending order.
+
+
+def binary_search(nums_list, search_number):
+    nums_list.sort()  # sort the list
+
+    while nums_list:
+        mid_index = len(nums_list) // 2  # find the middle index.
+        if nums_list[mid_index] == search_number:
+            return True
+        elif nums_list[mid_index] > search_number:
+            nums_list = nums_list[: mid_index]
+        elif nums_list[mid_index] < search_number:
+            nums_list = nums_list[mid_index + 1:]
+    return False
+
+
+print(binary_search(numbers_list, 17))
+
+
