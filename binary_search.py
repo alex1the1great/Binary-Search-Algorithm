91626a703de2924bb75e26f11d75d881c5504399
Asim Shrestha <asimshrestha608@gmail.com>
2021-05-04 15:51:22 +0545

    feat: generate list of numbers using lower limit and upper limit given by user

--- binary_search.py
+++ binary_search.py
@@ -1,3 +1,8 @@
 user_number = int(input('Enter a number for computer to guess: '))
 user_lower_bound = int(input('Enter lower bound: '))
 user_upper_bound = int(input('Enter upper bound: '))
+
+
+def generate_list(lower_bound, upper_bound):
+    return (list(range(lower_bound, upper_bound + 1)))
+
