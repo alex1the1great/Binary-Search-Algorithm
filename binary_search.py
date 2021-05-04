4fea1abe41171df1d64ac5efa70d5d337f9f3066
Asim Shrestha <asimshrestha608@gmail.com>
2021-05-04 13:56:02 +0545

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,24 +1,26 @@
-from random import shuffle
+import random
 
-# creating list of numbers from 0 to 9.
-numbers_list = [i for i in range(10)]
-shuffle(numbers_list)  # shuffle the numbers list.
+def generate_random_list(l_bound, u_bound):
+    new_list = []
+    for _ in range(u_bound - l_bound):
+        new_list.append(random.randint(l_bound, u_bound))
 
+    new_list = set(new_list)
+    new_list = list(new_list)
+    return new_list
 
-def binary_search(nums_list, search_number):
-    nums_list.sort()  # sort the list
+lower_bound = 5
+upper_bound = 100
+search_list = sorted(generate_random_list(lower_bound, upper_bound))
+computer_choice = random.choice(search_list)
+print(computer_choice)
 
-    while nums_list:
-        mid_index = len(nums_list) // 2  # find the middle index.
-        if nums_list[mid_index] == search_number:
-            return True
-        elif nums_list[mid_index] > search_number:
-            nums_list = nums_list[: mid_index]
-        elif nums_list[mid_index] < search_number:
-            nums_list = nums_list[mid_index + 1:]
-    return False
+print(f"Guess between {lower_bound} to {upper_bound} included both ranges.")
 
+def binary_search(s_list, c_choice):
+    user_input = int('Enter your guess: ')
 
-print(binary_search(numbers_list, 17))
-
-
+    if user_input == c_choice:
+        print(f"Congratulation! {user_input} is the correct number.")
+    elif user_input > c_choice:
+        s_list = s_list[]
